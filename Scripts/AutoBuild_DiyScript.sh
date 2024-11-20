b98305e1a25abfc335d0c7dbc6b27cf034d769fb
knight-stars <112705101+knight-stars@users.noreply.github.com>
2024-11-20 22:07:22 +0800

    Update AutoBuild_DiyScript.sh

--- Scripts/AutoBuild_DiyScript.sh
+++ Scripts/AutoBuild_DiyScript.sh
@@ -49,6 +49,7 @@ Firmware_Diy() {
 	# ${FEEDS_CONF}			OpenWrt 源码目录下的 feeds.conf.default 文件
           # git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
           # git clone https://github.com/sirpdboy/luci-app-poweroffdevice package/luci-app-poweroffdevice
+	   git clone https://github.com/sirpdboy/luci-app-netdata package/luci-app-netdata
 	   git clone https://github.com/knight-stars/openwrt-packages package/openwrt-packages
 	# ${CustomFiles}			仓库中的 /CustomFiles 绝对路径
 	# ${Scripts}				仓库中的 /Scripts 绝对路径
