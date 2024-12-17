222aef119f5ebc9ffe50ac559ebd9b8eda9a4d9e
nobodyman-star <290378374@qq.com>
2024-12-18 07:30:10 +0800

    Update AutoBuild_DiyScript.sh

--- Scripts/AutoBuild_DiyScript.sh
+++ Scripts/AutoBuild_DiyScript.sh
@@ -55,15 +55,15 @@ Firmware_Diy() {
            git clone https://github.com/nobodyman-star/luci-app-onliner.git
 	   echo "src-git packages https://github.com/coolsnowwolf/packages" > feeds.conf.default
            echo "src-git luci https://github.com/coolsnowwolf/luci" >> feeds.conf.default
-           echo "#src-git luci https://github.com/coolsnowwolf/luci.git;openwrt-23.05 >> feeds.conf.default
-           echo "src-git routing https://github.com/coolsnowwolf/routing >> feeds.conf.default
-           echo "src-git telephony https://github.com/coolsnowwolf/telephony.git >> feeds.conf.default
-           echo "src-git helloworld https://github.com/fw876/helloworld.git >> feeds.conf.default
-           echo " #src-git oui https://github.com/zhaojh329/oui.git >> feeds.conf.default
-           echo "#src-git video https://github.com/openwrt/video.git >> feeds.conf.default
-           echo "#src-git targets https://github.com/openwrt/targets.git >> feeds.conf.default
-           echo "#src-git oldpackages http://git.openwrt.org/packages.git >> feeds.conf.default
-           echo "#src-link custom /usr/src/openwrt/custom-feed >> feeds.conf.default
+           echo "#src-git luci https://github.com/coolsnowwolf/luci.git;openwrt-23.05" >> feeds.conf.default
+           echo "src-git routing https://github.com/coolsnowwolf/routing" >> feeds.conf.default
+           echo "src-git telephony https://github.com/coolsnowwolf/telephony.git" >> feeds.conf.default
+           echo "src-git helloworld https://github.com/fw876/helloworld.git" >> feeds.conf.default
+           echo " #src-git oui https://github.com/zhaojh329/oui.git" >> feeds.conf.default
+           echo "#src-git video https://github.com/openwrt/video.git" >> feeds.conf.default
+           echo "#src-git targets https://github.com/openwrt/targets.git" >> feeds.conf.default
+           echo "#src-git oldpackages http://git.openwrt.org/packages.git" >> feeds.conf.default
+           echo "#src-link custom /usr/src/openwrt/custom-feed" >> feeds.conf.default
                 
         
 	# ${CustomFiles}			仓库中的 /CustomFiles 绝对路径
